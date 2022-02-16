fb0798e34751666d8179f4cb45cde30e477069bf
MarcosReboucasDS <marcos.silva1343@etec.sp.gov.br>
2022-02-16 10:49:30 +0900

    Correção text

--- www/lib/des.js
+++ www/lib/des.js
@@ -1,11 +1,13 @@
-
     function usar(){
         var a;
+        var vl;
         var p;
         var imc;
         a = parseFloat(document.getElementById("altura").value);  
         p = parseFloat(document.getElementById("peso").value);
+        vl = parseFloat(document.getElementById("valor").value);
         imc = p/(a*a);
+        valor.value = imc;
         if(imc < 18.5){
             alert(imc);
             document.getElementById('foto').src = "lib/fotos/abaixo.png";
